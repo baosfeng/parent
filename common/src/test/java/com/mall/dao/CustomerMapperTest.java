455cea1612cb894d5267982e9e6ec583863715e9
smallbag <bsfeng@foxmail.com>
2018-12-18 08:49:07 +0800

    对customer进行测试

--- common/src/test/java/com/mall/dao/CustomerMapperTest.java
+++ common/src/test/java/com/mall/dao/CustomerMapperTest.java
@@ -7,17 +7,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("classpath:/applicationContext.xml")
 public class CustomerMapperTest {
 
 	@Autowired
 	private CustomerMapper customerMapper;
-
+	@Test
+	public void testInsert() {
+		Customer customer = new Customer();
+		customer.setUserName("admin");
+		customer.setPassword("abc123");
+		customer.setDisplayName("admin");
+		customer.setRegisterTime(new Date());
+		int insert = customerMapper.insert(customer);
+		assertEquals(1, insert);
+	}
 	@Test
 	public void selectTest() {
 		Customer customer = customerMapper.selectByPrimaryKey(1);
-		System.out.println(customer);
+		assertNotEquals(null, customer);
 	}
-
+	@Test
+	public void deleteTest() {
+		int i = customerMapper.deleteByPrimaryKey(4);
+		assertEquals(1, i);
+	}
 }
